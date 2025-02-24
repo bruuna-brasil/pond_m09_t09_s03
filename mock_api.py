d8cdc29241114c96c29f3520f2882c20c84a5063
bruuna-brasil <124185478+bruuna-brasil@users.noreply.github.com>
2025-02-24 08:30:49 -0300

    upgrade: using cache

--- mock_api.py
+++ mock_api.py
@@ -1,11 +1,18 @@
+from flask_caching import Cache
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/taxa', methods=['GET'])
-def get_taxa():
-    return jsonify({"taxa": 12.5})
+# Configuração do Redis
+app.config['CACHE_TYPE'] = 'RedisCache'  # Usa Redis como cache
+app.config['CACHE_REDIS_URL'] = "redis://localhost:6379/0"  # URL do Redis na porta 6379
 
+cache = Cache(app)
+
+@app.route('/taxa')
+@cache.cached(timeout=2)  # Cache por 60 segundos
+def get_taxa():
+    return jsonify({"taxa": 10.5})
 
 if __name__ == '__main__':
     app.run(port=5000)
